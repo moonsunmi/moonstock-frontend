cad22d948e3e389ceaa17caca63b14b15b65859c
moonsunmi <celebmoon@naver.com>
2024-03-20 14:44:43 +0900

    fix: replace font import to _document from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,9 @@
 import "app/globals.css";
 import { AppProps } from "next/app";
-import Head from "next/head";
-import "@fontsource/roboto/300.css"; // Light
-import "@fontsource/roboto/400.css"; // Regular
-import "@fontsource/roboto/500.css"; // Medium
-import "@fontsource/roboto/700.css"; // Bold
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import type { DocumentContext, DocumentInitialProps } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
@@ -29,5 +30,27 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+          />
+        </Head>{" "}
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
