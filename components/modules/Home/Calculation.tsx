e0ac3231e72c80177c1b07119f0287dffb077550
moonsunmi <celebmoon@naver.com>
2024-03-21 14:52:04 +0900

    refactor: make parent for StockInforBoard component

--- components/modules/Home/Calculation.tsx
+++ components/modules/Home/Calculation.tsx
@@ -1,59 +1,14 @@
-import styled from "@emotion/styled";
-import { Box } from "@mui/material";
-import NumberInput from "./NumberInput";
-import { ColorTypes, GrayColors } from "styles/colors";
-import { Span1 } from "@/components/typographies";
-import useInvestment from "hooks/useInvestment";
+import { PrimaryColors } from "styles/colors";
+import StockInfoBoard from "./StockInfoBorad";
 
-type Props = {
-  title: string;
-  bgColor?: ColorTypes;
-};
-
-const Calculation = ({ title, bgColor = GrayColors.gray100 }: Props) => {
-  const { inputs, total, handleChange } = useInvestment();
+const Calculation = () => {
   return (
-    <Wrapper bgColor={bgColor}>
-      <Span1>{title}</Span1>
-      <Box
-        component="form"
-        display="flex"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <NumberInput
-          id="price"
-          name="price"
-          label="가격"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-        <NumberInput
-          id="quantity"
-          name="quantity"
-          label="수량"
-          value={inputs.quantity}
-          onChange={handleChange}
-        />
-        <NumberInput
-          id="total"
-          name="total"
-          label="총합"
-          value={total}
-          aria-readonly
-        />
-      </Box>
-    </Wrapper>
+    <>
+      <StockInfoBoard title="보유 주식" />
+      <StockInfoBoard title="추가 매수" />
+      <StockInfoBoard title="최종 보유" bgColor={PrimaryColors.var100} />
+    </>
   );
 };
-
-const Wrapper = styled.div<{ bgColor: ColorTypes }>`
-  background-color: ${(props) => props.bgColor};
-  padding: 5px;
-  margin: 5px;
-`;
 
 export default Calculation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,20 @@
-import Button from "@/components/Button";
 import NavBar from "@/components/layouts/Gnb/NavBar";
 import PageWrapper from "@/components/layouts/PageWrapper/PageWrapper";
 import Calculation from "@/components/modules/Home/Calculation";
 import StockList from "@/components/modules/Home/StockList";
 import styled from "@emotion/styled";
-import { PrimaryColors } from "styles/colors";
+// import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <PageWrapper>
       <NavBar />
-      <Calculation title="보유 주식" />
-      <Calculation title="추가 매수" />
-      <Calculation title="최종 보유" bgColor={PrimaryColors.var100} />
-      <ButtonWrapper>
+      <Calculation />
+      {/* <ButtonWrapper>
         <Button>코스피</Button>
         <Button>코스닥</Button>
         <Button>나스닥</Button>
-      </ButtonWrapper>
+      </ButtonWrapper> */}
       <StockList />
     </PageWrapper>
   );
