a9d67da2a95f22ecdb589b88d3d6050676cb6c65
moonsunmi <celebmoon@naver.com>
2024-04-07 16:30:02 +0900

    feat: implement API route for fetching stock information from external service

--- components/modules/Home/AddPurchase.tsx
+++ components/modules/Home/AddPurchase.tsx
@@ -1,23 +1,12 @@
 import { useStockContext } from "@/contexts/stockContext/StockContext";
 import { createInitialPurchase } from "@/contexts/stockContext/initialStocks";
-import { Box, Button, Grid, TextField } from "@mui/material";
-import instance from "api/apiClient";
+import { Grid, TextField } from "@mui/material";
 import { ChangeEvent, useCallback, useState } from "react";
-import styled from "styled-components";
 import { ActionType } from "types/actionTypes";
 import { apiStatus } from "types/apiStatus";
 import { Purchase, StockInfoType } from "types/stockTypes";
 import StatusDescription from "./StatusDescription";
 import StyledButton from "./StyledButton";
-
-const getStockInfo = async (stockName: string) => {
-  try {
-    const response = await instance.get("", { params: { itmsNm: stockName } });
-    return response.data.response.body;
-  } catch (error) {
-    throw new Error("서버 응답 에러");
-  }
-};
 
 const AddPurchase = () => {
   const [stockName, setStockName] = useState<string>("");
@@ -35,7 +24,10 @@ const AddPurchase = () => {
     setStatus(apiStatus.loading);
     setStockName("");
     try {
-      const data: StockInfoType = await getStockInfo(stockName);
+      const response = await fetch(
+        `/api/getStockInfo?stockName=${encodeURIComponent(stockName)}`
+      );
+      const data: StockInfoType = await response.json();
       if (data && data.totalCount > 0) {
         const newPrice = data.items?.item[0]?.clpr;
         if (newPrice) {
@@ -61,7 +53,7 @@ const AddPurchase = () => {
   };
 
   return (
-    <Grid container spacing={1} width="100vw" sx={{ padding: 1 }}>
+    <Grid container spacing={1} sx={{ padding: 1 }}>
       <Grid item xs={12} sm={6}>
         <TextField
           size="small"
