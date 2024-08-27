3179d4cc1d41bef327db6c66b3e55c1d51f6175c
sm.moon <sm.moon0716@milliway.co.kr>
2024-08-27 09:34:37 +0900

    refactor: change type location

---
+++ src/@types/actionTypes.ts
@@ -0,0 +1,24 @@
+import {IPurchase} from './stockTypes'
+
+export enum ActionType {
+  ADD = 'add',
+  UPDATE = 'update',
+  REMOVE = 'remove'
+}
+
+interface AddPurchase {
+  type: ActionType.ADD
+  payload: IPurchase
+}
+
+interface UpdatePurchase {
+  type: ActionType.UPDATE
+  payload: IPurchase
+}
+
+interface RemovePurchase {
+  type: ActionType.REMOVE
+  payload: {id: string}
+}
+
+export type PurchaseAction = AddPurchase | UpdatePurchase | RemovePurchase
