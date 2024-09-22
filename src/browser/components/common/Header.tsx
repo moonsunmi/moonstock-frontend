edf63b19bc5ed56e89d845bc5e96809c9ba1e2d4
moonsunmi <celebmoon@naver.com>
2024-09-22 16:13:08 +0900

    fix: resolve hydration error

--- src/browser/components/common/Header.tsx
+++ src/browser/components/common/Header.tsx
@@ -4,10 +4,16 @@ import {useRouter} from 'next/navigation'
 // Components
 import {Button} from '../UI'
 import {useSelector} from '@/store/store'
+import {useEffect, useState} from 'react'
 
 export const Header = () => {
   const router = useRouter()
   const {userInfo} = useSelector(state => state.auth)
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
 
   return (
     <div className="fixed flex flex-row content-center justify-between w-full h-16 p-5 font-bold shadow-md text-primary-950 bg-secondary-200">
@@ -17,7 +23,7 @@ export const Header = () => {
       <div className="flex gap-1">
         <Button variant="text">Components</Button>
         <Button variant="text" onClick={() => router.push('/login')}>
-          {userInfo?.id === null ? 'Login' : 'LogOut'}
+          {isMounted ? (userInfo?.id === null ? 'LogIn' : 'LogOut') : 'LogIn'}
         </Button>
       </div>
     </div>
