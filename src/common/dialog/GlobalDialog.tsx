a5def19c9d491c464bc475cb1536a74f1c008fd6
moonsunmi <celebmoon@naver.com>
2025-06-06 21:17:58 +0900

    chores: add console log to check error

--- src/common/dialog/GlobalDialog.tsx
+++ src/common/dialog/GlobalDialog.tsx
@@ -2,7 +2,14 @@ import DeleteDialog from './DeleteDialog'
 import TradeDialog from './TradeDialog'
 
 export const getStockList = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/stocks`)
+  const backendUrl = process.env.BACKEND_URL
+  console.log('✅ BACKEND_URL:', backendUrl)
+
+  if (!backendUrl) {
+    throw new Error('BACKEND_URL is not defined')
+  }
+
+  const res = await fetch(`${backendUrl}/stocks`)
 
   if (!res.ok) throw new Error('Failed to fetch stock list')
   return res.json()
